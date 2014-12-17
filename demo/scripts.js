c5995ade02d571235adb210418db8dd16fe2a8b0
zzuieliyaoli <zzuieliyaoli@gmail.com>
2014-12-17 16:57:29 +0800

    vol 1

--- demo/scripts.js
+++ demo/scripts.js
@@ -4,12 +4,8 @@ window.onload = function(){
 		line = document.getElementById("wrapper").querySelector("span.move"),
 		nowNum = 0;
 
-	// beginLeft 怎样记录
 	$each(liArray,function(o,i){
-		// var num = i;
-		// console.log(num)
-		// IE9 事件处理程序  P373 mouseover mouseout
-		// var beginLeft = ??????
+		
 		var endLeft = i * 154;
 		o.addEventListener("click", function(){
 			nowNum = i;
@@ -19,7 +15,6 @@ window.onload = function(){
 			clearTimeout(tId);
 			var tId = setTimeout(function(){
 				line.style.left = endLeft + "px";
-				// leftMove(o,beginLeft,endLeft);
 			},20);
 		}, false);
 
@@ -31,12 +26,6 @@ window.onload = function(){
 		}, false);
 	});
 
-	// 动画效果
-
-	// 移入移出
-
-	// 点击链接，跳转后，被选定
-
 	function $each(arr, callback, thisp){
 		if (arr.forEach) {
 			arr.forEach(callback,thisp);
@@ -47,27 +36,9 @@ window.onload = function(){
 		}
 	}
 
-	function leftMove(elem, begin, end){
-		var elemLeft = begin;
-		console.log(begin);
-		var tId = setTimeout(function(){
-
-			if (elemLeft >= end) {
-				clearTimeout(tId);
-				elem.style.left = end + "px";
-			}else{
-				elemLeft = (begin + (elemLeft / 6 )) + "px";
-				elem.style.left = elemLeft;
-				// console.log("moveLeft");
-				// console.log(elemLeft);
-				setTimeout(arguments.callee, 20);
-			}
-			
-		},20);
-	}
+	
 
 
 
 };
 
-// css3动画
